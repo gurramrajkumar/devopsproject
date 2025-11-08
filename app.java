2376a2fdba05516891e3302f88340621b5c65cc9
gurramrajkumar <rockstarraj380@gmail.com>
2025-11-08 20:31:56 +0530

    Update dev2 code

--- app.java
+++ app.java
@@ -3,3 +3,21 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+//dev2 added code
+class Main {
+  public static void main(String[] args) {
+
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
